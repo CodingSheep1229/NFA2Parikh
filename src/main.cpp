5a901776cac63b23eb114b6237b48ac3a6b7a8de
Yang Cheng <b05705014@ntu.edu.tw>
2018-08-23 14:56:10 +0800

    Change file layout

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include "NFA.h"
+#include "ExpTree.h"
+#include <fstream>
+
+using namespace std;
+
+int main(int argc,char* argv[])
+{
+    string filename = argv[1];
+    
+    fstream file;
+
+    file.open(filename);
+
+    string str = "",inputStr;
+
+    while(getline(file, inputStr))
+    {
+        str += inputStr;
+        str += "\n";
+    }
+    file.close();
+
+    NFA nfa(str);
+
+    cout << nfa.ParikhImage() << endl;
+
+    return 0;
+}
